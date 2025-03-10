5f67d4115bfb96ae51a7f8b01729b29df3d6fef9
Ömer Can <omercanhocaoglu@gmail.com>
2025-03-10 18:10:36 +0300

    ejs ve page işlemleri tamamlandı.

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Template engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-    res.status(200).send("index sayfası");
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+app.get("/about", (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+});
+app.get("/contact", (req, res) => {
+    res.status(200).render('contact', {
+        page_name: "contact"
+    });
 });
 
 app.listen(port, () => {
