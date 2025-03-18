5b60840f3a06e9c26002bfafc01f76b32da5effc
Ömer Can <omercanhocaoglu@gmail.com>
2025-03-18 16:30:16 +0300

    kurs modeli oluşturuldu.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageControllers = require("./controllers/pageControllers");
-const pageRouter = require ("./routes/pageRoute");
+const pageRouter = require("./routes/pageRoute");
+const courseRouter = require("./routes/courseRoute");
 
 const app = express();
 const port = 3000;
+
+// Connect DB
+mongoose.connect('mongodb://localhost/misfit-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }).then(() => {
+    console.log('db connected succesfully!');
+});
 
 // Template engine
 app.set("view engine", "ejs");
@@ -13,6 +25,7 @@ app.use(express.static("public"));
 
 // Routes
 app.use("/", pageRouter );
+app.use("/courses", courseRouter );
 
 
 app.listen(port, () => {
