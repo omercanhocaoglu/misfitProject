5f76c1861378dd53e29da89ed7836f50a1a56b56
Ömer Can <omercanhocaoglu@gmail.com>
2025-03-20 18:04:18 +0300

    kurs listeleme işi tamamlanmıştır.

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,18 +1,18 @@
 // home page
 exports.getIndexPage = (req, res) => {
     res.status(200).render('index', {
-        page_name: "index"
+        page_name: 'index'
     });
 };
 // about page
 exports.getAboutPage = (req, res) => {
     res.status(200).render('about', {
-        page_name: "about"
+        page_name: 'about'
     });
 };
 // contact page
 exports.getContactPage = (req, res) => {
     res.status(200).render('contact', {
-        page_name: "contact"
+        page_name: 'contact'
     });
 };
